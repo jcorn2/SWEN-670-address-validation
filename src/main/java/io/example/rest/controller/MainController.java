26a616d5e0c9020560b43d33d644b001664ff1c2
James Cornelius <jcorn2@umbc.edu>
2020-09-23 06:14:35 -0400

    Have parameters for address

--- src/main/java/io/example/rest/controller/MainController.java
+++ src/main/java/io/example/rest/controller/MainController.java
@@ -3,6 +3,7 @@ package io.example.rest.controller;
 import io.example.rest.service.ParsingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,18 +11,20 @@ import java.util.LinkedHashMap;
 
 @RestController
 public class MainController {
-    private static final String JSON_GEOCODE = "https://maps.googleapis.com/maps/api/geocode/json?address=317+Carriage+Run+Road,+Annapolis,+MD,+21403&key=<insert key here>";
+    private static final String JSON_GEOCODE = "http://www.mapquestapi.com/geocoding/v1/address?key=";
 
     @Autowired
     private ParsingService parsingService;
 
     @GetMapping("/geocode")
-    public LinkedHashMap main() {
-        LinkedHashMap response = (LinkedHashMap) parsingService.parse(JSON_GEOCODE);
+    public LinkedHashMap main(@RequestParam(name = "address", required = true) String address) {
+        String url = JSON_GEOCODE + address;
+        LinkedHashMap response = (LinkedHashMap) parsingService.parse(url);
         ArrayList<Object> results = (ArrayList<Object>) response.get("results");
         LinkedHashMap topResult = (LinkedHashMap) results.get(0);
-        LinkedHashMap geometry = (LinkedHashMap) topResult.get("geometry");
-        LinkedHashMap latAndLong = (LinkedHashMap) geometry.get("location");
-        return latAndLong;
+        ArrayList<Object> locations = (ArrayList<Object>) topResult.get("locations");
+        LinkedHashMap addressInfo = (LinkedHashMap) locations.get(0);
+        LinkedHashMap latLng = (LinkedHashMap) addressInfo.get("latLng");
+        return latLng;
     }
 }
