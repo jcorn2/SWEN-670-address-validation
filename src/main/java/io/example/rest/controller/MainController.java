4873b9d52c9e7a31cb59d9f2ffd8dea8ca265835
James Cornelius <jcorn2@umbc.edu>
2020-10-01 18:44:05 -0400

    Convert code to POST request

--- src/main/java/io/example/rest/controller/MainController.java
+++ src/main/java/io/example/rest/controller/MainController.java
@@ -1,10 +1,12 @@
 package io.example.rest.controller;
 
+import io.example.rest.domain.Location;
 import io.example.rest.service.ParsingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import zones.MapPoint;
@@ -15,16 +17,17 @@ import java.util.List;
 
 @RestController
 public class MainController {
-    private static final String JSON_GEOCODE = "http://www.mapquestapi.com/geocoding/v1/address?key=&location=";
+    private static final String JSON_GEOCODE = "http://www.mapquestapi.com/geocoding/v1/address?key=" +
+            "&location=";
     MapPoint startApp = new MapPoint();
 
     @Autowired
     private ParsingService parsingService;
 
-    @GetMapping("/geocode")
-    public LinkedHashMap main(@RequestParam(name = "address", required = true) String address) throws Exception {
+    @RequestMapping(value = "/geocode", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public LinkedHashMap main(@RequestBody Location userLocation) throws Exception {
         LinkedHashMap latLng;
-        String mapquestUrl = JSON_GEOCODE + address;
+        String mapquestUrl = JSON_GEOCODE + userLocation.getLocation();
         LinkedHashMap addressInfo;
         String geocodeQualityCode;
 
